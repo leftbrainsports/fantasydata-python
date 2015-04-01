08a5f094c651ea60af61c844e1aebe4d1582da6d
Igor Polyakov <igor.v.polyakov@gmail.com>
2015-04-01 06:21:27 -0400

    Current version

--- create_python_venv.sh
+++
@@ -1,2 +0,0 @@
-pip install virtualenv
-virtualenv venv
--- tests/TestFantasyData.py
+++ tests/TestFantasyData.py
@@ -1,11 +1,12 @@
 # content of test_sysexit.py
 import pytest
-from FantasyData import FantasyData, FantasyDataError
-
-api_key = '19944f3e54d84a5a93918aa35bbe5abf'
-invalid_api_key = '19944f3e54d84a5a93918aa35bbe5ab_'
+from fantasy_data.FantasyData import FantasyData, FantasyDataError
 
 class TestFantasyData:
+    
+    def read_api_key(self):
+        with open("api_key.txt", "r") as api_key_file:
+            return api_key_file.read().replace('\n', '')
     
     '''
     Happy path
@@ -19,7 +20,8 @@ class TestFantasyData:
         It returns the integer 2015
     '''
     def test_get_upcoming_season_happy_path(self):
-        global api_key
+        api_key = self.read_api_key()
+        print api_key
         fantasy_data = FantasyData(api_key)
         assert (fantasy_data.get_upcoming_season() == 2015)
     
@@ -35,7 +37,7 @@ class TestFantasyData:
         It throws an exception stating "Error: Cannot connect to the FantasyData API"
     '''
     def test_get_upcoming_season_api_is_unavailable(self):
-        global invalid_api_key
+        invalid_api_key = '19944f3e54d84a5a93918aa35bbe5ab_'
         fantasy_data = FantasyData(invalid_api_key)
         with pytest.raises(FantasyDataError): 
             fantasy_data.get_upcoming_season()
@@ -52,12 +54,12 @@ class TestFantasyData:
         It throws an exception stating "Error: Invalid API key"
     '''
     def test_get_upcoming_season_invalid_api_key(self):
-        global invalid_api_key
+        invalid_api_key = '19944f3e54d84a5a93918aa35bbe5ab_'
         fantasy_data = FantasyData(invalid_api_key)
         with pytest.raises(FantasyDataError): 
             fantasy_data.get_upcoming_season()
     
     def test_get_schedules_for_season(self):
-        global api_key
+        api_key = self.read_api_key()
         fantasy_data = FantasyData(api_key)
         fantasy_data.get_schedules_for_season(2014)
