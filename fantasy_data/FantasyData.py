1ff0f76ca3ec21898e01261475aefc82d3987aeb
Kevin Day <kday33@gmail.com>
2017-08-27 13:04:39 -0400

    Add NFL get_projected_player_game_stats_by_player

--- fantasy_data/FantasyData.py
+++ fantasy_data/FantasyData.py
@@ -157,6 +157,13 @@ class FantasyData(FantasyDataBase):
         result = self._method_call("Player/{player_id}", player_id=player_id)
         return result
 
+    def get_projected_player_game_stats_by_player(self, season, week, player_id):
+        """
+        Projected Player Game Stats by Player
+        """
+        result = self._method_call("PlayerGameProjectionStatsByPlayerID/{season}/{week}/{player_id}", season=season, week=week, player_id=player_id)
+        return result
+
 class FantasyDataNBA(FantasyDataBase):
     """
     Class provide Fantasy Data API calls (NFL)
